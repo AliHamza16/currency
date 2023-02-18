3b25316ab6211694665f4750814461dd3c061110
AliHamza16 <alihamzakrbg@gmail.com>
2023-02-18 14:19:44 +0300

    1.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CurrencyItem v-for="currency in currency_rates" :code="currency[0]"
+  :title="titles[currency[0]]" :value="currency[1]" v-bind:key="currency[0]"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import CurrencyItem from './components/Currency.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    CurrencyItem,
+  },
+  data() {
+    return {
+      currency_rates: null,
+      titles: {
+        USD: "ABD Doları",
+        EUR: "Euro",
+        GBP: "İngiliz Sterlini",
+        CHF: "İsviçre Frangı",
+        CAD: "Kanada Doları",
+        RUB: "Rus Rublesi",
+        AED: "Birleşik Arap Emirlikleri Dirhemi",
+        AUD: "Avustralya Doları",
+        DKK: "Danimarka Kronu",
+        SEK: "İsveç Kronu",
+        NOK: "Norveç Kronu",
+        JPY: "Japon Yeni",
+        KWD: "Kuveyt Dinarı",
+        ZAR: "Güney Afrika Randı",
+        BHD: "Bahreyn Dinarı",
+        LYD: "Libya Dinarı",
+      },
+    };
+  },
+  mounted() {
+    fetch('https://finans.truncgil.com/v3/today.json')
+      .then((response) => response.json())
+      .then((data) => {
+        this.currency_rates = data;
+        delete this.currency_rates.Update_Date;
+        Object.keys(this.currency_rates).map((key) => {
+          if (this.currency_rates[key].Type !== "Currency" || !this.currency_rates[key].Buying) {
+            delete this.currency_rates[key];
+          }
+          return 0;
+        });
+        this.currency_rates = Object.keys(this.currency_rates).map((key) => [`${key}`, this.currency_rates[key].Selling]);
+      });
+    setInterval(() => {
+      fetch('https://finans.truncgil.com/v3/today.json')
+        .then((response) => response.json())
+        .then((data) => {
+          this.currency_rates = data;
+          delete this.currency_rates.Update_Date;
+          Object.keys(this.currency_rates).map((key) => {
+            if (this.currency_rates[key].Type !== "Currency" || !this.currency_rates[key].Buying) {
+              delete this.currency_rates[key];
+            }
+            return 0;
+          });
+          this.currency_rates = Object.keys(this.currency_rates).map((key) => [`${key}`, this.currency_rates[key].Selling]);
+        });
+    }, 100000);
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
---
+++ src/components/Currency.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="currency-box">
+    <img :src="flag" :alt="code" class="flag">
+    <div class="title-box">
+      <span class="code">{{ code }}</span>
+      <span class='title' id="title">{{ titleD }}</span>
+    </div>
+    <div class="value-box">
+      <span class="value">{{ value }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CurrencyItem",
+  props: {
+    title: String,
+    value: String,
+    code: String,
+  },
+  data() {
+    return {
+      flag: `flags/${this.code}.png`,
+      hover: '',
+      titleD: this.title,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.currency-box {
+  display: flex;
+  width: 400px;
+  background-color: #d4d4d4;
+  border-radius: 20px;
+  user-select: none;
+  transition: .3s;
+  margin: 20px;
+}
+
+.currency-box:hover {
+  width: 450px;
+}
+
+.currency-box .flag {
+  width: 80px;
+  height: 80px;
+  border-radius: 20px;
+}
+
+.currency-box .title-box {
+  margin-left: 1.5em;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.currency-box .title-box .code {
+  font-weight: bold;
+}
+
+.currency-box .title-box .title {
+  text-overflow: ellipsis;
+  text-align: left;
+}
+
+.currency-box .value-box {
+  display: flex;
+  margin-left: auto;
+  margin-right: 1em;
+  align-items: center;
+  justify-content: center;
+}
+
+.currency-box .value-box .value {
+  padding: 1em;
+  background-color: rebeccapurple;
+  border-radius: 10px;
+  color: white;
+}
+</style>
